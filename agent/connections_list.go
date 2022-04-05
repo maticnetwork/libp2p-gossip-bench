566acd4b928696ccd8d583dde14d10f585732952
Igor Crevar <crewce@gmail.com>
2022-04-05 10:14:25 +0200

    POS3-74 Random network creation (#7)

---
+++ agent/connections_list.go
@@ -0,0 +1,35 @@
+package agent
+
+import (
+	"time"
+
+	"github.com/maticnetwork/libp2p-gossip-bench/utils"
+)
+
+// Holds list of all connection pairs (source and destination ClusterAgent)
+type connectionsList []struct {
+	src  ClusterAgent
+	dest ClusterAgent
+}
+
+// Creates new connectionsList
+func NewConnectionsList() connectionsList {
+	return make(connectionsList, 0)
+}
+
+// Add new connection pair to the list
+func (cl *connectionsList) Add(src, dest ClusterAgent) {
+	*cl = append(*cl, struct {
+		src  ClusterAgent
+		dest ClusterAgent
+	}{src: src, dest: dest})
+}
+
+// Connects all connection pairs (added via Add func)
+// Execute Connect operations in multiple routines because of speed
+func (cl connectionsList) ConnectAll() (int, int, time.Duration) {
+	return utils.MultiRoutineRunner(len(cl), func(index int) error {
+		ch := cl[index]
+		return ch.src.Connect(ch.dest)
+	})
+}
