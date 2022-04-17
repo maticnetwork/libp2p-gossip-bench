fb24f12cc480423c2d3851168a9b7b199cf05f80
momosh-ethernal <momcilo@ethernal.tech>
2022-04-17 09:53:19 +0200

    added stream tests for next and changes

--- observer/stream_test.go
+++ observer/stream_test.go
@@ -1,6 +1,9 @@
 package observer
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func TestStreamInitialValue(t *testing.T) {
 	try := "yo"
@@ -13,10 +16,11 @@ func TestStreamInitialValue(t *testing.T) {
 
 func TestStreamUpdate(t *testing.T) {
 	init := "yo"
-	update := "po"
 	state := newState(init)
-	updatedState := state.update(update)
 	stream := &stream{state: state}
+
+	update := "po"
+	updatedState := state.update(update)
 
 	if val := stream.Value(); val != init {
 		t.Errorf("Expected to get %#v but got %#v\n", init, val)
@@ -25,5 +29,69 @@ func TestStreamUpdate(t *testing.T) {
 	updatedState.update("ndoshta")
 	if val := stream.Value(); val != init {
 		t.Errorf("Expected to get %#v but got %#v\n", init, val)
+	}
+}
+
+func TestStreamNext(t *testing.T) {
+	init := "yo"
+	state := newState(init)
+	stream := &stream{state: state}
+
+	update := "po"
+	updatedState := state.update(update)
+
+	if val := stream.Next(); val != update {
+		t.Errorf("Expected to get %#v but got %#v\n", update, val)
+	}
+
+	newUpdate := "ndoshta"
+	updatedState.update(newUpdate)
+
+	if val := stream.Next(); val != newUpdate {
+		t.Errorf("Expected to get %#v but got %#v\n", update, val)
+	}
+}
+
+func TestStreamChanges(t *testing.T) {
+	init := "yo"
+	state := newState(init)
+	stream := &stream{state: state}
+
+	select {
+	case <-stream.Changes():
+		t.Errorf("Expecting nothing here")
+	default:
+	}
+
+	update := "po"
+	go func() {
+		time.Sleep(1 * time.Second)
+		state.update(update)
+	}()
+
+	select {
+	case <-stream.Changes():
+	// wait some period of time longer than 1 second needed for update
+	// if this happens 1st, we have a problem
+	case <-time.After(2 * time.Second):
+		t.Errorf("Shold be changes before this")
+	}
+
+	// state done channel is closed at this point
+	// we should still be able to receive from Changes
+	select {
+	case <-stream.Changes():
+	default:
+		t.Errorf("Should be changes here")
+	}
+
+	if val := stream.Next(); val != update {
+		t.Errorf("Expected to get %#v but got %#v\n", update, val)
+	}
+
+	select {
+	case <-stream.Changes():
+		t.Errorf("Should be no changes here")
+	default:
 	}
 }
